89ea6676cf78a5f4278c51eb4cb9dfd5f2c00e0e
Tikaum <tulkubai@rambler.ru>
2026-03-09 21:57:41 +0500

    219F

--- 3prog/task219F.pas
+++ 3prog/task219F.pas
@@ -1,35 +1,34 @@
 program task219F;
 var
 	c: char;	
-	n, long, space, count, countS : integer;
+	long, space, count, countS : integer;
 	itword, itspace: boolean;
 begin
-	n := 0;
 	long := 0;
 	space := 0;
 	count := 0;
 	countS := 0;
 	itword := false;
-	itspace := false;
+	itspace := true;
 
 	while not eof do
 	begin
 		read(c);
 		if c = #10 then 
 		begin
-			if itword then
-				n := n + 1;
 			if count > long then
 				long := count;
-			if (count < short) and (count > 0) then
-				short := count;			
-			writeln('Кол-во слов ', n, '. Длина длинного ', long,
-			 '. Длина короткого ', short);
+			write('Длина самого длинного слова ', long);
+			if countS > space then
+				space := countS;
+			writeln('. Длина самого длинного пробела ', space);			
+			
 			itword := false;
-			n := 0;
+			itspace := true;
 			long := 0;
-			short := 0;
-			count := 0			
+			count := 0;			
+			space := 0;
+			countS := 0
 		end
 		
 		else
@@ -37,17 +36,29 @@ begin
 			if (c <> ' ') then
 			begin
 				itword := true;
-				count := count + 1
+				itspace := false;
+				count := count + 1;
+				if countS > space then
+					space := countS;
+				countS := 0;				
 			end
 				
 			else if itword then
 			begin
-				n := n + 1;
 				itword := false;
+				itspace := true;
 				if count > long then
 					long := count;
-				count := 0				
+				count := 0;
+				countS := 1;			
 			end			
+
+			else if itspace then
+			begin
+				countS := countS + 1;
+				if countS > space then
+					space := countS;	
+			end
 		end	
 	end
 end.
