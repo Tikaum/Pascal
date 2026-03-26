803362c0044736f28539c29218e361625cb9fdf1
Tikaum <tulkubai@rambler.ru>
2026-03-26 23:45:36 +0500

    s224 end

--- 4prog/s224.pas
+++ 4prog/s224.pas
@@ -1,10 +1,9 @@
 program s224;
 
 var
-  s1, s2, s3, med, resstr: string;
+  s1, s2, s3, med: string;
   num1, num2, res: real;
-  num3, code, i, a, b: integer;
-  todot: boolean;
+  num3, code, i, a, b, dotpos: integer;
 begin
 	s1 := ParamStr(1);
 	s2 := ParamStr(2);
@@ -31,26 +30,27 @@ begin
 	
 	res := num1 * num2;
 	str(res:0:num3, med);
+	writeln(med);	
+
+	for i := 1 to Length(med) do
+	begin
+		if med[i] = '.' then
+			dotpos := i			
+	end;		
 
 	a := 0;
-	todot := false;
-
-	for i := Length(med) downto (Length(med) - 10) do
+	for i := Length(med) downto (dotpos + 1) do
 	begin
 		if med[i] = '0' then
 			a := a + 1
-		else if med[i] = '.' then
-			todot := true
 		else
 			break
 	end;			
-	
-	if (a > 0) and not todot then
-		b := Length(med) - a
-	else if todot then
-		b := Length(med) - (a + 1);
+
+	b := Length(med) - a;
+	if b = dotpos then
+		b := dotpos - 1;
 	
 	SetLength(med, b);
-	
 	writeln(med)
 end.
