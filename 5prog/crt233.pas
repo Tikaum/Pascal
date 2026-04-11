2becf2eb4551432b7f8bf3c3fed541a5edc496d4
Tikaum <tulkubai@rambler.ru>
2026-04-11 21:34:46 +0500

    begin 233

---
+++ 5prog/crt233.pas
@@ -0,0 +1,113 @@
+program crt233;
+uses crt;
+const 
+	DelayDuration = 100;
+	
+type 
+	star = record
+		CurX, CurY, dx, dy: integer;
+	end;
+
+procedure ShowStar (s: star);
+begin
+	GotoXY(s.CurX, s.CurY);
+	write('*');
+	GotoXY(1, 1)
+end;
+
+procedure HideStar (s: star);
+begin
+	GotoXY(s.CurX, s.CurY);
+	write(' ');
+	GotoXY(1, 1)
+end;
+
+procedure MoveStar (var s: star; var c: char);
+var
+	a, b, olddx, olddy: integer;
+	chan: boolean;
+begin
+	HideStar(s);
+	olddx := s.dx;
+	olddy := s.dy;
+	chan := false;
+
+	if KeyPressed then
+	begin
+		c := ReadKey;
+		if (c = #27) then
+			exit
+	end;
+
+	a := random(10);
+	
+	if (a = 5) then
+	begin
+		repeat
+		begin
+			b := random(3);
+			if (b = 0) then
+			begin
+				s.dx := -1;
+				s.dy := 0
+			end
+			else if (b = 1) then
+			begin
+				s.dx := 1;
+				s.dy := 0
+			end
+			else if (b = 2) then
+			begin
+				s.dx := 0;
+				s.dy := -1
+			end
+			else if (b = 3)
+			begin
+				s.dx := 0;
+				s.dy := 1
+			end;
+			if (olddx <> s.dx) or (olddy <> s.dy) then
+				chan := true
+		end
+		until not chan			
+	end;
+	
+	s.CurX := s.CurX + s.dx;
+	if s.CurX > ScreenWidth then
+		s.CurX := 1
+	else if s.CurX < 1 then
+		s.CurX := ScreenWidth;
+	s.CurY := s.CurY + s.dy;
+	if s.CurY > ScreenHeight then
+		s.CurY := 1
+	else if s.CurY < 1 then
+		s.CurY := ScreenHeight;
+	ShowStar(s)
+end;
+
+var
+	s: star;
+	c: char;
+	
+begin
+	randomize;
+	clrscr;
+	s.CurX := ScreenWidth div 2;
+	s.CurY := ScreenHeight div 2;
+	s.dx := 0;
+	s.dy := 1;
+	c := 'a';
+	ShowStar(s);
+	while true do
+	begin
+		if KeyPressed then
+		begin
+			c := ReadKey;
+			if (c = #27) then
+				Halt(1)
+		end;		
+		MoveStar(s, c);
+		delay(DelayDuration);
+	end;
+	clrscr
+end.
